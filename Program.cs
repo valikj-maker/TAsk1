e337212e377c7ed1cae7f8275e3fea3e95e907a1
unknown <valikj@code.edu.az>
2026-03-04 12:33:12 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//int number = 1;
+
+//if (number == 0)
+//{
+//    Console.WriteLine("reqem 0-dir");
+//}
+//else if (number > 0)
+//{
+//    Console.WriteLine("reqem musbetdir");
+//}
+//else
+//{
+//    Console.WriteLine("reqem menfidir");
+//}
+
+//if (number % 2 == 0)
+//{
+//    Console.WriteLine("reqem cut ededdir");
+//}
+//else
+//{
+//    Console.WriteLine("reqem tek ededdir");
+//}
+//int yas = 21;
+//    if (yas >=0 && yas<18 )
+//    Console.WriteLine("usaq");
+//else if (yas >= 18 && yas<65 )
+//    Console.WriteLine("yetiskin");
+//else
+//{
+//    Console.WriteLine("yasli");
+//}
+//double mebleg = 500;
+//if (mebleg < 100)
+//    Console.WriteLine(mebleg);
+//else if (mebleg >= 100 && mebleg < 500)
+//    Console.WriteLine(mebleg * 9 / 10);
+//else if (mebleg>=500)
+//Console.WriteLine(mebleg * 8 / 10);
+
+//double salary = 10;
+//double experience = 2;
+//if (experience >= 0 && experience < 2)
+//    Console.WriteLine("no bonus");
+//else if (experience >= 2 && experience < 5)
+//    Console.WriteLine(salary * 85 / 100);
+//else if (experience >= 5)
+//    Console.WriteLine(salary * 75/100);
+
+//double mesafe = 10;
+//double manat = 5;
+//if (mesafe >= 0 && mesafe < 5)
+//    Console.WriteLine(manat);
+//else if (mesafe >= 5 && mesafe < 15)
+//    Console.WriteLine(mesafe*0.8 + manat);
+//else if (mesafe >=15)
+//    Console.WriteLine(mesafe*0.6+manat);
